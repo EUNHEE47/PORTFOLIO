2dd635eabdfc97d1f51d94697462673c93a8493d
PARK EUN HEE <mii0329@naver.com>
2023-03-23 18:52:24 +0900

    secondCommit

--- script/index.js
+++ script/index.js
@@ -1,11 +1,12 @@
+let content = "안녕하세요! 신입 개발자 박은희 입니다.";
+let mainTyping = document.querySelector(".main-typing");
+let i = 0;
 
-// new TypeIt(".introduce", {
-//   strings: ["This is a great string.", "But here is a better one."],
-//   speed: 50,
-//   waitUntilVisible: true,
-// }).go();
-
-
-new TypeIt(".introduce", {
-  strings: "This is what will be typed!",
-}).go();
+function typingText() {
+  if (i < content.length) {
+    let txt = content.charAt(i);
+    mainTyping.innerHTML += txt == "\n" ? "<br/>" : txt;
+    i++;
+  }
+}
+setInterval(typingText, 80);
