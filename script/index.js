6136dcf6808fd4c408f927917ac6dee2ab55788a
PARK EUN HEE <mii0329@naver.com>
2023-03-23 16:37:30 +0900

    first commit

---
+++ script/index.js
@@ -0,0 +1,11 @@
+
+// new TypeIt(".introduce", {
+//   strings: ["This is a great string.", "But here is a better one."],
+//   speed: 50,
+//   waitUntilVisible: true,
+// }).go();
+
+
+new TypeIt(".introduce", {
+  strings: "This is what will be typed!",
+}).go();
