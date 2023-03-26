502903131e34b0f6796e99d3ae426fcc40357da9
PARK EUN HEE <mii0329@naver.com>
2023-03-27 01:14:26 +0900

    profile

--- script/index.js
+++ script/index.js
@@ -1,11 +1,11 @@
-let content = "안녕하세요! 신입 개발자 박은희 입니다.";
-let mainTyping = document.querySelector(".main-typing");
+let content = `안녕하세요! 신입 개발자 박은희 입니다.`;
+let profileTyping = document.querySelector(".profile-typing");
 let i = 0;
 
 function typingText() {
   if (i < content.length) {
     let txt = content.charAt(i);
-    mainTyping.innerHTML += txt == "\n" ? "<br/>" : txt;
+    profileTyping.innerHTML += txt == "\n" ? "<br/>" : txt;
     i++;
   }
 }
